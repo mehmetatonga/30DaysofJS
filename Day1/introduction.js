d9b2bbd58e6dcdf94e8ecc4e4f6d16dbb2a36c46
Mehmet Ali <mehmetatonga@gmail.com>
2024-03-15 15:39:42 +0300

    Initial commit

---
+++ Day1/introduction.js
@@ -0,0 +1,21 @@
+// console.log("Bu yazı introduction.js'den geldi.")
+
+console.log(typeof 5)
+console.log(typeof "Mehmet")
+console.log(typeof true)
+console.log(typeof null)
+
+// let youtubeChannel = "Mehmet Ali"
+// let channelAge = 10
+// let isChannelStillActive = true
+// let secondChannel = null
+// let channelMoney = undefined
+
+let youtubeChannel = "Mehmet Ali", channelAge = 10, isChannelStillActive = true, secondChannel = null, channelMoney = undefined
+
+const pi = 3.14
+const gravity = 9.81
+
+console.log(youtubeChannel, channelAge, isChannelStillActive, secondChannel, channelMoney)
+
+console.log(pi, gravity)
