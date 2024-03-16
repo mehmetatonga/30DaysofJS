8bd04799cac058d59ec4f7235be0e6274ff60082
Mehmet Ali <mehmetatonga@gmail.com>
2024-03-16 14:56:01 +0300

    16.03.2024, 14:55

---
+++ Day4/int.js
@@ -0,0 +1,24 @@
+let num = 5
+if (num>0){
+    console.log("The number is positive")
+}else if(num<0){
+    console.log("The number is negative")
+}else{
+    console.log("The number is zero")
+}
+
+// let sonuc = prompt("2 + 5 = ?") 
+// if(sonuc == "7"){ //if else ile popup quiz mantığı
+//    alert("Doğru cevapladın!")  
+// } else {
+//    alert("Yanlış cevapladın.")
+// }
+
+// switch(sonuc){  //Switch case ile popup quiz mantığı
+//     case "7":
+//         alert("Doğru cevapladın!");
+//         break;
+//     default:
+//         alert("Yanlış cevapladın.");
+// }
+
